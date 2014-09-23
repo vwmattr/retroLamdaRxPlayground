f94880512db3beb167a14fc3afd20b3647da0f5b
Matt R <vwmattr@gmail.com>
2014-09-22 21:15:08 -0400

    initial commit

---
+++ app/src/main/java/com/example/matt/retrolambdaplayground/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.matt.retrolambdaplayground;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView) findViewById(R.id.hello);
+        //'Bout as simple as it gets here.  {@link View.OnClickListener} interface has a single
+        //method that takes a single parameter View v
+        tv.setOnClickListener(v -> Toast.makeText(this, "Hello Lambda", Toast.LENGTH_SHORT).show());
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
